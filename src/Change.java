4612d403e82b8cf6c2391831ca8a804585279408
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-30 09:03:06 +0530

    solves money change problem

--- src/Change.java
+++ src/Change.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
-        //write your code here
-        return m;
+    private static int getChange(int money) {
+        int coins = money / 10;
+        money -= 10 * coins;
+        coins += money / 5;
+        money -= (money / 5) * 5;
+        return coins + money;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int m = scanner.nextInt();
         System.out.println(getChange(m));
-
     }
 }
 
