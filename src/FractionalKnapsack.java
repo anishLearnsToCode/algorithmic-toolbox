6a70d1c29f9dd74c50dbc095ebd62d023b8474be
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-30 09:22:40 +0530

    solves fractional knapsack

--- src/FractionalKnapsack.java
+++ src/FractionalKnapsack.java
@@ -1,23 +1,45 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        //write your code here
-
-        return value;
+    private static double getOptimalValue(int capacity, List<Item> items) {
+        items.sort(Comparator.reverseOrder());
+        double knapsack = 0;
+        for (int index = 0 ; index < items.size() && capacity > 0 ; index++) {
+            Item item = items.get(index);
+            knapsack += item.weight <= capacity ? item.value : item.ratio * capacity;
+            capacity -= Math.min(item.weight, capacity);
+        }
+        return knapsack;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int numberOfItems = scanner.nextInt();
         int capacity = scanner.nextInt();
-        int[] values = new int[n];
-        int[] weights = new int[n];
-        for (int i = 0; i < n; i++) {
-            values[i] = scanner.nextInt();
-            weights[i] = scanner.nextInt();
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < numberOfItems ; i++) {
+            items.add(new Item(scanner.nextInt(),scanner.nextInt()));
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        System.out.println(getOptimalValue(capacity, items));
+    }
+
+    private static class Item implements Comparable<Item> {
+        private final int weight;
+        private final int value;
+        private final double ratio;
+
+        Item(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+            this.ratio = (double) value / weight;
+        }
+
+        @Override
+        public int compareTo(Item other) {
+            return Double.compare(this.ratio, other.ratio);
+        }
     }
 } 
