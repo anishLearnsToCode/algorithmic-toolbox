4fc1bf4d2e506ab1f702f3f5b8539df23627c22a
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-30 10:57:57 +0530

    solves car fueling

--- src/CarFueling.java
+++ src/CarFueling.java
@@ -1,21 +1,37 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class CarFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
-        return -1;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static int computeMinRefills(int tank, int[] stops) {
+        int minStops = 0;
+        for (int index = 1, start = 0, previous = 0 ; index < stops.length ; index++) {
+            if (stops[index] - start > tank) {
+                if (previous == start && index != 1 || previous - start > tank) {
+                    return -1;
+                }
+                minStops++;
+                start = previous;
+            }
+            previous = stops[index];
+        }
+
+        return stops[stops.length - 1] - stops[stops.length - 2] <= tank ? minStops : -1;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int dist = scanner.nextInt();
+        int distance = scanner.nextInt();
         int tank = scanner.nextInt();
-        int n = scanner.nextInt();
-        int stops[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            stops[i] = scanner.nextInt();
-        }
+        int[] stops = getArray(scanner.nextInt(), distance);
+        System.out.println(computeMinRefills(tank, stops));
+    }
 
-        System.out.println(computeMinRefills(dist, tank, stops));
+    private static int[] getArray(int length, int distance) {
+        int[] array = new int[length + 2];
+        for (int index = 1 ; index <= length ; index++) {
+            array[index] = scanner.nextInt();
+        }
+        array[length + 1] = distance;
+        return array;
     }
 }
